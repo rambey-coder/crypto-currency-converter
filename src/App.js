ba7d8b5c06408b580ade3970ddecb79348434d2a
unknown <olayiwolaagboolaakanji@gmail.com>
2022-04-22 16:29:21 +0100

    directory change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import NewsFeed from './components/NewsFeed'
 import CurrencyConverter from './components/CurrencyConverter'
-import ExchangeRate from './components/ExchangeRate';
 
 function App() {
   return (
     <div className="App">
       <NewsFeed />
       <CurrencyConverter />
-      <ExchangeRate />
     </div>
   );
 }
--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,8 +1,12 @@
+import ExchangeRate from './components/ExchangeRate';
 import React from 'react'
 
 const CurrencyConverter = () => {
   return (
-    <div className='currency-converter'>CurrencyConverter</div>
+    <div className='currency-converter'>
+      <div></div>
+      <ExchangeRate />
+    </div>
   )
 }
 
